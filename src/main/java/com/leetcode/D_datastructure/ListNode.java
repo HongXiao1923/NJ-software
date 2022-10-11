6d533a1f2f7d4406db3fa6d3fe6fe0b7363f8dac
Einstein <matrix369.work@gmail.com>
2022-10-11 21:10:51 +0800

    20221011爱因斯坦

---
+++ src/main/java/com/leetcode/D_datastructure/ListNode.java
@@ -0,0 +1,15 @@
+package com.leetcode.D_datastructure;
+
+/**
+ * @author Einstein
+ * @version 1.0
+ * @description: Todo
+ * @date 2022/10/11 18:52
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+}
